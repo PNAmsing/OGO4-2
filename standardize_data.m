449e6e497a7186bc800f366c88c21b682e268b72
Vermeer <a.j.e.vermeer@student.tue.nl>
2018-12-20 12:14:19 +0100

    ..

--- standardize_data.m
+++ standardize_data.m
@@ -8,12 +8,5 @@ end
 mean_var = [mean_var,0]; std_var = [std_var,1];
 complete_casesTOT_stand = array2table(((table2array(complete_casesTOT)) - mean_var)./std_var);  
 
-for pat = 1:643
-    if complete_casesTOT_stand{pat,end} == 2 || complete_casesTOT_stand{pat,end} == 3
-        complete_casesTOT_stand{pat,end} = 'DO';
-    elseif complete_casesTOT_stand{pat,end}== 1 
-        complete_casesTOT_stand{pat,end} = 'NDO';
-    end
-end
 
 
